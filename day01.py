dd12d1f6ba4a600cc35fce19d62bb7354c530a88
Sam Bentley <sam.bentley@reece.com.au>
2024-12-02 18:24:13 +1100

    day 1 - solved

--- day01.py
+++ day01.py
@@ -4,12 +4,39 @@ from aocd import get_data
 
 data = get_data(day=1, year=2024).splitlines()
 
+# PART 1
+
 first_list = []
 second_list = []
 
 for i in range(len(data)):
-    first_list.append(data[i].split()[0])
-    second_list.append(data[i].split()[1])
+    row = data[i].split()
 
-print(first_list)
-print(second_list)
+for i, line in enumerate(data):
+    line = line.split()
+    first_list.append(int(line[0]))
+    second_list.append(int(line[1]))
+
+first_list.sort()
+second_list.sort()
+
+totals = 0
+
+for i in range(len(first_list)):
+    if first_list[i] > second_list[i]:
+        totals += first_list[i] - second_list[i]
+    else:
+        totals += second_list[i] - first_list[i]
+
+print(totals)
+
+# PART 2
+
+unique_nums = set(first_list)
+totals = 0
+
+for num in unique_nums:
+    appearances = second_list.count(num)
+    totals += num * appearances
+
+print(totals)
