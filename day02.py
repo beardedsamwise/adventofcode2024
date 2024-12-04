41f1d7fb3e663a1979d5ea37a4bd528e66e67805
Sam Bentley <sam.bentley@reece.com.au>
2024-12-04 13:36:52 +1100

    day 2 - part 1 solved

--- day02.py
+++ day02.py
@@ -4,40 +4,39 @@ from aocd import get_data
 
 data = get_data(day=2, year=2024).splitlines()
 
-safe_reports = 0
+list_of_data = []
 
 for line in data:
-    # print(line)
-    split_line = line.split()
+    line = line.split()
+    for i in range(len(line)):
+        line[i] = int(line[i])
+    list_of_data.append(line)
 
-    sorted_line = split_line.copy()
+safe_reports = 0
+
+print(list_of_data[896])
+
+for line in list_of_data:
+
+    sorted_line = line.copy()
     sorted_line.sort()
 
-    reverse_line = split_line.copy()
+    reverse_line = line.copy()
     reverse_line.sort(reverse=True)
 
-    line_length = len(split_line)
-    unique_items = len(set(split_line))
+    line_length = len(line)
+    unique_items = len(set(line))
 
-    if (
-        split_line == sorted_line or split_line == reverse_line
-    ) and line_length == unique_items:
-        # print(line)
-        # print(sorted_line)
-        SAFE_REPORT = True
-        for i in range(len(split_line) - 1):
-            # print("----")
-            # print(sorted_line[i + 1])
-            # print("MINUS")
-            # print(sorted_line[i])
-            # print("")
-            if int(sorted_line[i + 1]) - int(sorted_line[i]) > 3:
-                SAFE_REPORT = False
-        if not SAFE_REPORT:
-            print(f"Unsafe report: {line}")
-        else:
-            print(f"SAFE report: {line}")
+    if (line == sorted_line or line == reverse_line) and line_length == unique_items:
+
+        SAFE = True
+
+        for i in range(len(sorted_line) - 1):
+            diff = int(sorted_line[i + 1]) - int(sorted_line[i])
+            if diff > 3:
+                SAFE = False
+
+        if SAFE:
             safe_reports += 1
-
 
 print(safe_reports)
